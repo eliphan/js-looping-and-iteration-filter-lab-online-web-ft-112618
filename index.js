d3810059583620e2e8156e29900ee4ba01a52e25
Elise Phan <aliceconnic@gmail.com>
2019-03-14 03:09:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution in this file
 function findMatching(drivers, name) {
-  drivers.filter(function (name))
+  drivers.filter(function (name) {
+    name ==
+  })
 }
 
 [1, 2, 3, 4, 5].filter(function (num) { return num > 3; });
