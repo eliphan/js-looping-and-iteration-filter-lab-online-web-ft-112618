22da378b1a2ebb1011f5c31a19751ffc0e06b91e
Elise Phan <aliceconnic@gmail.com>
2019-03-14 03:17:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ function findMatching(drivers, string) {
 
 function fuzzyMatch(drivers, string) {
   let stringLength = string.length;
-  return drivers.filter(function (letter) {
-    return name.slice(0, stringLength) === name
+  return drivers.filter(function (name) {
+    return name.slice(0, stringLength) === string
   })
+}
+
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
 }
